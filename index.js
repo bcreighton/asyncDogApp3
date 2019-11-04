8926dd40b76e8167aa9d7b0f95521aba3a95c881
Bryson Creighton <brysoncreighton@gmail.com>
2019-11-03 22:06:34 -0700

    added functionality to generate by breed and handle 404 error

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 'use strict';
 
-function getDogImages(dogs){
-    fetch(`https://dog.ceo/api/breeds/image/random/${dogs}`)
+function getDogImages(breed, dogs){
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random/${dogs}`)
         .then(response => response.json())
         .then(responseJSON => {
-            const dogImages = responseJSON.message;
+            if(responseJSON.status == 'error'){
+                $('.dogImageContainer').replaceWith(
+                    `<p>${breed} does not exist; please refresh the page and try again.</p>`
+                 )
+            } else {
+                const dogImages = responseJSON.message;
 
-            for(let i=0; i<dogImages.length; i++){
-               $('.dogImageContainer').append(
-                   `<img src="${dogImages[i]}" class='dogImage'>`
-                )
+                for(let i=0; i<dogImages.length; i++){
+                $('.dogImageContainer').append(
+                    `<img src="${dogImages[i]}" class='dogImage'>`
+                    )
+                }
             }
         })
-        .catch(error => console.log('An error has occured; please try again.'));
-        
-        $('.dogImageContainer').removeClass('hidden');
+        .then($('.dogImageContainer').removeClass('hidden'))
+        .catch(error => console.log('An error has occured; please try again.'))
 }
 
 function formListener(){
     $('form').submit(event => {
         event.preventDefault();
 
+        const dogBreed = $('#breed').val();
         const numberOfDogs = $('#dogs').val();
-        getDogImages(numberOfDogs);
+        getDogImages(dogBreed, numberOfDogs);
     });
 }
 
